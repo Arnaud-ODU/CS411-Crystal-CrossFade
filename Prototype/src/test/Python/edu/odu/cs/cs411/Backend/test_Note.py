74b856aff2d10e49b4431962c987f62043e979b5
Joseph Wassell <jwass002@odu.edu>
2023-11-16 04:23:26 -0500

    Adjusted For Inability to Overload Functions in Python

--- Prototype/src/test/Python/edu/odu/cs/cs411/Backend/test_Note.py
+++ Prototype/src/test/Python/edu/odu/cs/cs411/Backend/test_Note.py
@@ -25,22 +25,31 @@ class Test_NoteMethods(unittest.TestCase):
     
 
     #Tests The Nondefault Constructor
-    def test_NondefaultConstructor(self):
-        note = Note('john', 1, 2, 3)
-        self.assertEqual(note.name, 'john')
+    #def test_NondefaultConstructor(self):
+    #    note = Note('john', 1, 2, 3)
+    #    self.assertEqual(note.name, 'john')
+    #    self.assertEqual(note.pitch, 1)
+    #    self.assertEqual(note.duration, 2)
+    #    self.assertEqual(note.amplitude, 3)
+    
+    #Tests The Copy Constructor
+    #def test_CopyConstructor(self):
+    #    note1 = Note('smith', 4, 5, 6)
+    #    note2 = Note(note1)
+    #    self.assertEqual(note1.name, note2.name)
+    #    self.assertEqual(note1.pitch, note2.pitch)
+    #    self.assertEqual(note1.duration, note2.duration)
+    #    self.assertEqual(note1.amplitude, note2.amplitude)
+    
+    #Tests The SetVars Function
+    def test_SetVars(self):
+        note = Note()
+        note.SetVars('John', 1, 2, 3)
+        self.assertEqual(note.name, 'John')
         self.assertEqual(note.pitch, 1)
         self.assertEqual(note.duration, 2)
         self.assertEqual(note.amplitude, 3)
-    
-    #Tests The Copy Constructor
-    def test_CopyConstructor(self):
-        note1 = Note('smith', 4, 5, 6)
-        note2 = Note(note1)
-        self.assertEqual(note1.name, note2.name)
-        self.assertEqual(note1.pitch, note2.pitch)
-        self.assertEqual(note1.duration, note2.duration)
-        self.assertEqual(note1.amplitude, note2.amplitude)
-    
+        
 
 #Runs All Tests Present In The File
 def test_Note():
