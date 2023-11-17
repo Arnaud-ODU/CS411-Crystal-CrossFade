5555173ce4afab1557e47ceba85c3c92e98ce0d3
Hyacinth Abad <habad002@odu.edu>
2023-11-16 19:50:26 -0500

    D

--- Prototype/src/main/Python/edu/odu/cs/cs411/Front-End/Menu.py
+++ Prototype/src/main/Python/edu/odu/cs/cs411/Front-End/Menu.py
@@ -6,3 +6,8 @@ def on_button_click():
 # Create the main window
 root = tk.Tk()
 root.title("My GUI")
+
+# Create widgets
+label = tk.Label(root, text="Enter your name:")
+entry = tk.Entry(root)
+button = tk.Button(root, text="Say Hello", command=on_button_click)
