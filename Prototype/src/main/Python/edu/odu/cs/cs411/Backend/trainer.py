42ade6eab895aad4f2748342d074ea93bc04e195
Mohamed Abdullahi <legitchaos23@gmail.com>
2023-11-17 23:15:09 -0500

    barebones trainer

--- Prototype/src/main/Python/edu/odu/cs/cs411/Backend/trainer.py
+++ Prototype/src/main/Python/edu/odu/cs/cs411/Backend/trainer.py
@@ -1,45 +1,33 @@
-# -*- coding: utf-8 -*-
-#Since we are using pytorch, this is a test code to see if its working for you
 import torch
-import math
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from music21 import *
+import os
 
+processed_notesData = []
+def process_musicXMLData(musicscore):
+    
+    
+    
+    
+    return processed_notesData
 
-dtype = torch.float
-device = torch.device("cpu")
-# device = torch.device("cuda:0") # Uncomment this to run on GPU
+class MusicXMLDataset(Dataset):
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        self.file_paths = [os.path.join(root_dir, file) for file in os.listdir(root_dir) if file.endswith(".xml")]
 
-# Create random input and output data
-x = torch.linspace(-math.pi, math.pi, 2000, device=device, dtype=dtype)
-y = torch.sin(x)
+    def __len__(self):
+        return len(self.file_paths)
 
-# Randomly initialize weights
-a = torch.randn((), device=device, dtype=dtype)
-b = torch.randn((), device=device, dtype=dtype)
-c = torch.randn((), device=device, dtype=dtype)
-d = torch.randn((), device=device, dtype=dtype)
+    def __getitem__(self, idx):
+        file_path = self.file_paths[idx]
+        score = converter.parse(file_path)
+        processed_notesData = process_musicXMLData(score)
+        return processed_notesData
 
-learning_rate = 1e-6
-for t in range(2000):
-    # Forward pass: compute predicted y
-    y_pred = a + b * x + c * x ** 2 + d * x ** 3
+root_directory = ""
+batch_size = 32
 
-    # Compute and print loss
-    loss = (y_pred - y).pow(2).sum().item()
-    if t % 100 == 99:
-        print(t, loss)
-
-    # Backprop to compute gradients of a, b, c, d with respect to loss
-    grad_y_pred = 2.0 * (y_pred - y)
-    grad_a = grad_y_pred.sum()
-    grad_b = (grad_y_pred * x).sum()
-    grad_c = (grad_y_pred * x ** 2).sum()
-    grad_d = (grad_y_pred * x ** 3).sum()
-
-    # Update weights using gradient descent
-    a -= learning_rate * grad_a
-    b -= learning_rate * grad_b
-    c -= learning_rate * grad_c
-    d -= learning_rate * grad_d
-
-
-print(f'Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3')
+dataset = MusicXMLDataset(root_directory)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
