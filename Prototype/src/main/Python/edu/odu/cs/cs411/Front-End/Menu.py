12bced5c3ebe3c1b45c18d895926303cc4f92776
Hyacinth Abad <habad002@odu.edu>
2023-11-17 21:15:10 -0500

    D

--- Prototype/src/main/Python/edu/odu/cs/cs411/Front-End/Menu.py
+++ Prototype/src/main/Python/edu/odu/cs/cs411/Front-End/Menu.py
@@ -1,21 +1,41 @@
 import tkinter as tk
+from tkinter import ttk, filedialog
 
-def on_button_click():
-    label.config(text="Hello, " + entry.get())
+class Editor:
+    def __init__(self, master, main_menu_callback):
+        self.master = master
+        self.master.title("Editor Window")
+        self.main_menu_callback = main_menu_callback  # Callback to main menu
 
-# Create the main window
-root = tk.Tk()
-root.title("My GUI")
+        # Create a notebook for tabs
+        self.notebook = ttk.Notebook(master)
 
-# Create widgets
-label = tk.Label(root, text="Enter your name:")
-entry = tk.Entry(root)
-button = tk.Button(root, text="Say Hello", command=on_button_click)
+        # Create frames for each tab
+        self.main_menu_frame = tk.Frame(self.notebook)
+        self.profile_frame = tk.Frame(self.notebook)
+        self.settings_frame = tk.Frame(self.notebook)
 
-# Organize widgets using grid layout
-label.grid(row=0, column=0, padx=10, pady=10)
-entry.grid(row=0, column=1, padx=10, pady=10)
-button.grid(row=1, column=0, columnspan=2, pady=10)
+        # Add frames to the notebook with corresponding tab names
+        self.notebook.add(self.main_menu_frame, text="Main Menu")
+        self.notebook.add(self.profile_frame, text="Profile")
+        self.notebook.add(self.settings_frame, text="Settings")
+        self.notebook.add(self.settings_frame, text="Tutorial")
 
-# Start the main event loop
-root.mainloop()
+        self.notebook.pack(fill=tk.BOTH, expand=True)
+
+        # Add buttons to the Editor frame
+        self.import_button = tk.Button(self.main_menu_frame, text="Import File", command=self.on_import_button_click)
+        self.import_button.pack()
+
+        self.open_last_project_button = tk.Button(self.main_menu_frame, text="Open Last Project", command=self.open_last_project)
+        self.open_last_project_button.pack()
+
+        # Additional buttons for Profile and Settings
+        self.profile_button = tk.Button(self.profile_frame, text="Profile Button", command=self.on_profile_button_click)
+        self.profile_button.pack()
+
+        self.settings_button = tk.Button(self.settings_frame, text="Settings Button", command=self.on_settings_button_click)
+        self.settings_button.pack()
+
+        # Main Menu tab in Editor
+        self.notebook.bind("<Button-1>", self.on_tab_click)  # Bind left mouse click to the tab
