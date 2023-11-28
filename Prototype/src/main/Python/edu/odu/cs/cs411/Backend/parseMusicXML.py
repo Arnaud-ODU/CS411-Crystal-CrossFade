a871af34a0f90d1440fee1de94c171d54af1e8c1
Dylan <charaxio@gmail.com>
2023-11-28 18:04:12 -0500

    Added and correct some docstrings

--- Prototype/src/main/Python/edu/odu/cs/cs411/Backend/parseMusicXML.py
+++ Prototype/src/main/Python/edu/odu/cs/cs411/Backend/parseMusicXML.py
@@ -4,25 +4,37 @@ from music21 import  *
 # Mohamed Abdullahi
 # CS411
  
- # Extract music information and present it as a string
 def parsemusic_XML(file):
+    """Extract music information from a MusicXML file.
+    
+    Args:
+        file: The MusicXML file being extracted from.
+        note_info: Information including pitch, duration type,
+        duration-quarter length, tie, pitch space, and octave, from an
+        individual note.
+    
+    Returns:
+        notation_string: A concatenated string containing all of the information 
+        from note_info. pitch.ps is used to distinguish between sharps and flats.
+        complete_stringfinal: A list of every notation_string from concat_musicinfo.
+    """
     score = converter.parse(file)
-    complete_stringfinal = [] # Create a list with all notes with their info
-    # We want a string of pitch, duration type, duration-quarter length, tie ,pitch space and octave
+    complete_stringfinal = [] 
+
     def concat_musicinfo(note_info):
-        notation_string = ''; # Collect all note info into one string
+        notation_string = ''; 
         
-        tie_string=''; # Will return start, continue or stop if there are tied notes
-        if x.tie != None: # Check if notes are tied or not
-           tie_string = x.tie.type;
+        tie_string=''; 
+        if x.tie != None: 
+            """Check if notes are tied or not"""
+            tie_string = x.tie.type;
        
-        #pitch.ps can distinguish between sharps and flats making it better than pitch.midi
         notation_string = str(note_info.pitch) + ", " + str(note_info.duration.type) + ", " + str(note_info.duration.quarterLength)+", " +str(tie_string) + ", " + str(note_info.pitch.ps) + ", " + str(note_info.octave);
          
         return notation_string
     
     for note in score.recurse().notes:
-       #Check if note is a note or chord(A collection of 3 notes or more being played at the same time)
+        """Check if note is a note or chord (a collection of three notes or more being played at the same time)"""
         if (note.isNote):
              x = note;
              complete_stringfinal.append(concat_musicinfo(x))
