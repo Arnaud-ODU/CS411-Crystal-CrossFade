470a63322c91de60c5273beec590533f9519ef9b
Hyacinth Abad <habad002@odu.edu>
2023-12-03 21:25:06 -0500

    2

--- Prototype/src/main/Python/edu/odu/cs/cs411/Front-End/Menu.py
+++ Prototype/src/main/Python/edu/odu/cs/cs411/Front-End/Menu.py
@@ -23,3 +23,27 @@ class Editor:
         self.notebook.add(self.settings_frame, text="Settings")
 
         self.notebook.pack(fill=tk.BOTH, expand=True)
+        
+        # Add buttons to the Editor frame
+        self.import_button = tk.Button(self.main_menu_frame, text="Import File", command=self.on_import_button_click)
+        self.import_button.pack()
+
+        self.open_last_project_button = tk.Button(self.main_menu_frame, text="Open Last Project", command=self.open_last_project)
+        self.open_last_project_button.pack()
+
+        # Canvas widget to display PDF file content
+        self.canvas = tk.Canvas(self.main_menu_frame, width=600, height=800)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        # Scrollbar
+        self.scrollbar = tk.Scrollbar(self.main_menu_frame, orient=tk.VERTICAL, command=self.canvas.yview)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        # Additional buttons for Profile and Settings
+        self.profile_button = tk.Button(self.profile_frame, text="Profile Button", command=self.on_profile_button_click)
+        self.profile_button.pack()
+
+        self.settings_button = tk.Button(self.settings_frame, text="Settings Button", command=self.on_settings_button_click)
+        self.settings_button.pack()
