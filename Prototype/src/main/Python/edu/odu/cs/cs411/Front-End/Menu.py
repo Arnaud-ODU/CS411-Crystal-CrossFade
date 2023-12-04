cf810ddb9aa8d889b85e0b9ba66d70a37cfe7b6c
habad002 <91424587+habad002@users.noreply.github.com>
2023-12-03 22:51:26 -0500

    Update Menu.py

--- Prototype/src/main/Python/edu/odu/cs/cs411/Front-End/Menu.py
+++ Prototype/src/main/Python/edu/odu/cs/cs411/Front-End/Menu.py
@@ -59,7 +59,7 @@ def open_editor_window():
     root.iconify()
 
     # Import the other script
-    from Main import App as OtherApp
+    from main import App as OtherApp
 
     # Create a new Tk instance for the second window
     second_window = tk.Toplevel()
@@ -71,30 +71,31 @@ def open_editor_window():
     # Start the second window's event loop
     second_window.mainloop()
 
-
 # Create the main window
 root = tk.Tk()
 root.title("CrossFade Main Menu")
 root.configure(bg=black)
 
+# Main Menu label above the logo
+label = tk.Label(root, text="Main Menu", font=("Arial", 14, "bold"), bg=black, fg=white)
+label.grid(row=1, column=1, pady=10)
+
 # Load and display a resized image on the Main Menu page
-image_path = "CF.png"  # Use a raw string
+image_path = r"C:\Users\hyaci\OneDrive\Pictures\CF.png"  # Use a raw string
 original_image = tk.PhotoImage(file=image_path)
 resized_image = original_image.subsample(2, 2)  # Adjust the subsample values for resizing
 image_label = tk.Label(root, image=resized_image, bg=black)
-image_label.grid(row=0, column=1, pady=10, sticky="nsew")  # Span two rows for the image
+image_label.grid(row=2, column=1, pady=10, sticky="nsew")  # Span two rows for the image
 
 # Create widgets
-label = tk.Label(root, text="Main Menu", font=("Arial", 14, "bold"), bg=black, fg=white)
 settings_button = tk.Button(root, text="Settings", command=open_editor_window, padx=20, pady=10, bg=dark_blue, fg=white, font=("Arial", 12, "bold"))
 profile_button = tk.Button(root, text="Profile", command=open_editor_window, padx=20, pady=10, bg=dark_blue, fg=white, font=("Arial", 12, "bold"))
 editor_button = tk.Button(root, text="Editor", command=open_editor_window, padx=20, pady=10, bg=dark_blue, fg=white, font=("Arial", 12, "bold"))  # Added Editor button
 
 # Organize widgets using grid layout
-label.grid(row=1, column=1, padx=10, pady=10)  # Increased columnspan to accommodate the Editor button
-settings_button.grid(row=2, column=0, pady=10)
-profile_button.grid(row=2, column=2, pady=10)
-editor_button.grid(row=3, column=1, pady=10)  # Placed the Editor button in the third column
+settings_button.grid(row=3, column=0, pady=10)
+profile_button.grid(row=3, column=2, pady=10)
+editor_button.grid(row=4, column=1, pady=10)  # Placed the Editor button in the third column
 
 # Configure row and column weights to make them expand with the window
 for i in range(5):  # Number of rows in the layout
@@ -103,3 +104,26 @@ for i in range(5):  # Number of rows in the layout
 
 # Start the main event loop
 root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
