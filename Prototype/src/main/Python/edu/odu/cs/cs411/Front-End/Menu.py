5637f7d3236f53bd4dea9f26aac7e400a2eb1140
habad002 <91424587+habad002@users.noreply.github.com>
2023-12-03 22:21:27 -0500

    Update Menu.py

--- Prototype/src/main/Python/edu/odu/cs/cs411/Front-End/Menu.py
+++ Prototype/src/main/Python/edu/odu/cs/cs411/Front-End/Menu.py
@@ -1,52 +1,35 @@
 import tkinter as tk
 from tkinter import ttk, filedialog
-import fitz  # PyMuPDF
-from PIL import Image, ImageTk
+
+# Define shades of black and blue
+black = "#000000"
+dark_blue = "#001F3F"
+white = "#FFFFFF"
 
 class Editor:
     def __init__(self, master, main_menu_callback):
         self.master = master
         self.master.title("Editor Window")
-        self.main_menu_callback = main_menu_callback  # Callback to the main menu
 
         # Create a notebook for tabs
         self.notebook = ttk.Notebook(master)
 
         # Create frames for each tab
-        self.main_menu_frame = tk.Frame(self.notebook)
-        self.profile_frame = tk.Frame(self.notebook)
-        self.settings_frame = tk.Frame(self.notebook)
+        self.main_menu_frame = tk.Frame(self.notebook, bg=black)
 
         # Add frames to the notebook with corresponding tab names
         self.notebook.add(self.main_menu_frame, text="Main Menu")
-        self.notebook.add(self.profile_frame, text="Profile")
-        self.notebook.add(self.settings_frame, text="Settings")
 
         self.notebook.pack(fill=tk.BOTH, expand=True)
-        
-        # Add buttons to the Editor frame
-        self.import_button = tk.Button(self.main_menu_frame, text="Import File", command=self.on_import_button_click)
-        self.import_button.pack()
 
-        self.open_last_project_button = tk.Button(self.main_menu_frame, text="Open Last Project", command=self.open_last_project)
-        self.open_last_project_button.pack()
+        # Import button in Editor frame
+        self.import_button = tk.Button(self.main_menu_frame, text="Import File", command=self.on_import_button_click, bg=dark_blue, fg=white)
+        self.import_button.pack(pady=10)
 
-        # Canvas widget to display PDF file content
-        self.canvas = tk.Canvas(self.main_menu_frame, width=600, height=800)
-        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-        # Scrollbar
-        self.scrollbar = tk.Scrollbar(self.main_menu_frame, orient=tk.VERTICAL, command=self.canvas.yview)
-        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-        self.canvas.configure(yscrollcommand=self.scrollbar.set)
-
-        # Additional buttons for Profile and Settings
-        self.profile_button = tk.Button(self.profile_frame, text="Profile Button", command=self.on_profile_button_click)
-        self.profile_button.pack()
-
-        self.settings_button = tk.Button(self.settings_frame, text="Settings Button", command=self.on_settings_button_click)
-        self.settings_button.pack()
+        # Configure row and column weights to make them expand with the window
+        for i in range(5):  # Number of rows in the layout
+            self.main_menu_frame.grid_rowconfigure(i, weight=1)
+            self.main_menu_frame.grid_columnconfigure(i, weight=1)
 
         # Main Menu tab in Editor
         self.notebook.bind("<Button-1>", self.on_tab_click)  # Bind left mouse click to the tab
@@ -60,64 +43,59 @@ class Editor:
             self.main_menu_callback()
 
     def on_import_button_click(self):
-        file_path = filedialog.askopenfilename(title="Select a PDF file to import", filetypes=[("PDF files", "*.pdf")])
+        file_path = filedialog.askopenfilename(title="Select a file to import")
         if file_path:
-            # Display the PDF file on the canvas
-            self.display_pdf(file_path)
-
-    def display_pdf(self, file_path):
-        try:
-            pdf_document = fitz.open(file_path)
-            for page_num in range(pdf_document.page_count):
-                page = pdf_document[page_num]
-                image = page.get_pixmap()
-                image = Image.frombytes("RGB", (image.width, image.height), image.samples)
-                photo = ImageTk.PhotoImage(image)
-                self.canvas.create_image(0, 0, anchor=tk.NW, image=photo)
-                self.canvas.image = photo  # Keep a reference to the image to prevent it from being garbage collected
-
-            pdf_document.close()
-            self.canvas.config(scrollregion=self.canvas.bbox("all"))
-        except Exception as e:
-            print(f"Error displaying PDF file: {e}")
-
-    def open_last_project(self):
-        # Add logic to open the last project
-        print("Opening last project...")
-
-    def on_profile_button_click(self):
-        # Add your profile logic here
-        print("Profile clicked")
-
-    def on_settings_button_click(self):
-        # Add your settings logic here
-        print("Settings clicked")
+            # Update the label or perform other actions as needed
+            print("Imported file:", file_path)
 
 # Callback function to go back to the main menu
-def go_to_main_menu():
+def go_to_main_menu(second_window):
+    second_window.destroy()  # Close the second window
     root.deiconify()  # Show the main menu window
 
 # Function to open the editor window
 def open_editor_window():
-    global editor_window
-    editor_window = tk.Toplevel(root)
-    editor = Editor(editor_window, go_to_main_menu)
+    # Hide the main menu window
+    root.iconify()
+
+    # Create a new Tk instance for the second window
+    second_window = tk.Toplevel()
+    second_window.title("Editor Window")
+
+    # Create the Editor instance
+    editor = Editor(second_window, lambda: go_to_main_menu(second_window))
+
+    # Start the second window's event loop
+    second_window.mainloop()
 
 # Create the main window
 root = tk.Tk()
 root.title("CrossFade Main Menu")
+root.configure(bg=black)
+
+# Load and display a resized image on the Main Menu page
+image_path = r"C:\Users\hyaci\OneDrive\Pictures\CF.png"  # Use a raw string
+original_image = tk.PhotoImage(file=image_path)
+resized_image = original_image.subsample(2, 2)  # Adjust the subsample values for resizing
+image_label = tk.Label(root, image=resized_image, bg=black)
+image_label.grid(row=0, column=1, pady=10, sticky="nsew")  # Span two rows for the image
 
 # Create widgets
-label = tk.Label(root, text="Options")
-settings_button = tk.Button(root, text="Settings", command=open_editor_window)
-profile_button = tk.Button(root, text="Profile", command=open_editor_window)
-editor_button = tk.Button(root, text="Editor", command=open_editor_window)  # Added Editor button
+label = tk.Label(root, text="Main Menu", font=("Arial", 14, "bold"), bg=black, fg=white)
+settings_button = tk.Button(root, text="Settings", command=open_editor_window, padx=20, pady=10, bg=dark_blue, fg=white, font=("Arial", 12, "bold"))
+profile_button = tk.Button(root, text="Profile", command=open_editor_window, padx=20, pady=10, bg=dark_blue, fg=white, font=("Arial", 12, "bold"))
+editor_button = tk.Button(root, text="Editor", command=open_editor_window, padx=20, pady=10, bg=dark_blue, fg=white, font=("Arial", 12, "bold"))  # Added Editor button
 
 # Organize widgets using grid layout
-label.grid(row=0, column=0, padx=10, pady=10, columnspan=3)  # Increased columnspan to accommodate the Editor button
-settings_button.grid(row=1, column=0, padx=10, pady=10)
-profile_button.grid(row=1, column=1, padx=10, pady=10)
-editor_button.grid(row=1, column=2, padx=10, pady=10)  # Placed the Editor button in the third column
+label.grid(row=1, column=1, padx=10, pady=10)  # Increased columnspan to accommodate the Editor button
+settings_button.grid(row=2, column=0, pady=10)
+profile_button.grid(row=2, column=2, pady=10)
+editor_button.grid(row=3, column=1, pady=10)  # Placed the Editor button in the third column
+
+# Configure row and column weights to make them expand with the window
+for i in range(5):  # Number of rows in the layout
+    root.grid_rowconfigure(i, weight=1)
+    root.grid_columnconfigure(i, weight=1)
 
 # Start the main event loop
 root.mainloop()
