6b2c196112d04bfc14808f0305271b39ae8a25d9
Hyacinth Abad <habad002@odu.edu>
2023-12-03 21:24:26 -0500

    1

--- Prototype/src/main/Python/edu/odu/cs/cs411/Front-End/Menu.py
+++ Prototype/src/main/Python/edu/odu/cs/cs411/Front-End/Menu.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 from tkinter import ttk, filedialog
-from musicxml import *
+import fitz  # PyMuPDF
+from PIL import Image, ImageTk
 
 class Editor:
     def __init__(self, master, main_menu_callback):
         self.master = master
         self.master.title("Editor Window")
-        self.main_menu_callback = main_menu_callback  # Callback to main menu
+        self.main_menu_callback = main_menu_callback  # Callback to the main menu
 
         # Create a notebook for tabs
         self.notebook = ttk.Notebook(master)
@@ -20,78 +21,5 @@ class Editor:
         self.notebook.add(self.main_menu_frame, text="Main Menu")
         self.notebook.add(self.profile_frame, text="Profile")
         self.notebook.add(self.settings_frame, text="Settings")
-        #self.notebook.add(self.settings_frame, text="Tutorial")
 
         self.notebook.pack(fill=tk.BOTH, expand=True)
-
-        # Add buttons to the Editor frame
-        self.import_button = tk.Button(self.main_menu_frame, text="Import File", command=self.on_import_button_click)
-        self.import_button.pack()
-
-        self.open_last_project_button = tk.Button(self.main_menu_frame, text="Open Last Project", command=self.open_last_project)
-        self.open_last_project_button.pack()
-
-        # Additional buttons for Profile and Settings
-        self.profile_button = tk.Button(self.profile_frame, text="Profile Button", command=self.on_profile_button_click)
-        self.profile_button.pack()
-
-        self.settings_button = tk.Button(self.settings_frame, text="Settings Button", command=self.on_settings_button_click)
-        self.settings_button.pack()
-
-        # Main Menu tab in Editor
-        self.notebook.bind("<Button-1>", self.on_tab_click)  # Bind left mouse click to the tab
-
-    def on_tab_click(self, event):
-        # Check if the click is on the "Main Menu" tab
-        current_tab = self.notebook.tk.call(self.notebook._w, "identify", "tab", event.x, event.y)
-        if "Main Menu" in current_tab:
-            # If so, go back to the main menu
-            self.master.destroy()
-            self.main_menu_callback()
-
-    def on_import_button_click(self):
-        file_path = filedialog.askopenfilename(title="Select a file to import")
-        if file_path:
-            # Update the label or perform other actions as needed
-            print("Imported file:", file_path)
-
-    def open_last_project(self):
-        # Add logic to open the last project
-        print("Opening last project...")
-
-    def on_profile_button_click(self):
-        # Add your profile logic here
-        print("Profile clicked")
-
-    def on_settings_button_click(self):
-        # Add your settings logic here
-        print("Settings clicked")
-
-# Callback function to go back to the main menu
-def go_to_main_menu():
-    root.deiconify()  # Show the main menu window
-
-# Function to open the editor window
-def open_editor_window():
-    global editor_window
-    editor_window = tk.Toplevel(root)
-    editor = Editor(editor_window, go_to_main_menu)
-
-# Create the main window
-root = tk.Tk()
-root.title("CrossFade Main Menu")
-
-# Create widgets
-label = tk.Label(root, text="Options")
-settings_button = tk.Button(root, text="Settings", command=open_editor_window)
-profile_button = tk.Button(root, text="Profile", command=open_editor_window)
-editor_button = tk.Button(root, text="Editor", command=open_editor_window)  # Added Editor button
-
-# Organize widgets using grid layout
-label.grid(row=0, column=0, padx=10, pady=10, columnspan=3)  # Increased columnspan to accommodate the Editor button
-settings_button.grid(row=1, column=0, padx=10, pady=10)
-profile_button.grid(row=1, column=1, padx=10, pady=10)
-editor_button.grid(row=1, column=2, padx=10, pady=10)  # Placed the Editor button in the third column
-
-# Start the main event loop
-root.mainloop()
