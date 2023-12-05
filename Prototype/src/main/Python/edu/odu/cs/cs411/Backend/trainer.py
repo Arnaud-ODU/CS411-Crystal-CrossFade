4bcac9b8e90318d3ac4c6da2e7af56eec2b915a0
Mohamed Abdullahi <legitchaos23@gmail.com>
2023-12-04 20:23:13 -0500

    Revised NN model

--- Prototype/src/main/Python/edu/odu/cs/cs411/Backend/trainer.py
+++ Prototype/src/main/Python/edu/odu/cs/cs411/Backend/trainer.py
@@ -1,3 +1,19 @@
+# Trainer Class
+# This Class Will Be Used To Train With Corrected Beam Folder
+# And Use The Evalution Model to predict Beam Errors From Uploaded MXL,XML,MusicXML 
+# Files For the User to see.
+#
+#
+# A Trainer Has:
+#  Pre-Processing of Data - to be prepared to be used as inputs and labels and turned to tensors
+#  DataSet Class - Access to Folders from training and testing folders and applys the transformations from raw data to tensors
+#  Neural Network Model - Structure of the machine learning and important for the behaviour and result of your model.
+#  Training - Uses the model and trains it with the training dataset
+#  Evalutation - Uses the model and tests it with the training dataset
+#@author Mohamed Abdullahi, Arnaud 
+#CS 411W Prof. Kennedy
+
+
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -28,7 +44,7 @@ def parse_beams(beam_data):
 def parse_musicxml(file_path):
     # Parsing musicXML file to extract features
     score = converter.parse(file_path) # Music21 toolkit to parse mxl/musicXML files
-    note_features = [] # Collection of all note for each note/chord
+    collection_features = [] # Collection of all note for each note/chord
    
     for note in score.flat.notes:
         # Extract features for each note or chord
@@ -38,9 +54,9 @@ def parse_musicxml(file_path):
          note_features = [note.measureNumber, note.offset, sum(p.midi for p in note.pitches) / len(note.pitches), note.duration.quarterLength]
         # Add beam info to the note features for each note/chord
         note_features.extend(parse_beams(note.beams))
-        note_features.append(note_features)    
+        collection_features.append(note_features)    
     
-    return note_features # Return value to be used in Dataset class
+    return collection_features # Return value to be used in Dataset class
 
 class MusicXMLDataset(Dataset):
     def __init__(self, directory):
@@ -59,32 +75,36 @@ class MusicXMLDataset(Dataset):
         data_item = self.data[idx]
         inputs = data_item[:4] # Use the first four as input [Measure Number, Note Offset, Note pitch, Note Duration]
         # The Beam information will be used as labels for the note information. Labels will be 1 for beam and 0 for no beam.
-        if int(data_item[4] > 0):  # If Beam Start is 1, make label as 1.
+        if int(data_item[4] > 0):  # If Beam 'Start' is 1, make label as 1.
             label = torch.tensor(int(data_item[4] > 0), dtype=torch.float32)       
-        elif int(data_item[5] > 0): # If Beam Continue is 2, make label as 1.
+        elif int(data_item[5] > 0): # If Beam 'Continue' is 2, make label as 1.
             label = torch.tensor(1, dtype=torch.float32)          
-        elif int(data_item[6] > 0): #If Beam Stop is 3, make label as 1
+        elif int(data_item[6] > 0): #If Beam 'Stop' is 3, make label as 1
              label = torch.tensor(1, dtype=torch.float32)
         else:                       # Else make label as 0.
             label = torch.tensor(0,dtype=torch.float32)
         #print(inputs, label)                                 
-        return inputs, label
+        return inputs, label # Returns Values for trainer
 
-#RNN Model for padding uneven sequence lengths for tensors    
-class RNNModel(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-       super(RNNModel, self).__init__()
-       self.embedding = nn.Embedding(input_size, hidden_size)
-       self.rnn = nn.RNN(hidden_size, hidden_size, batch_first=True)
-       self.linear = nn.Linear(hidden_size, output_size)
-    
-    def forward(self, x, lengths):
-       embedded = self.embedding(x)
-       packed = pack_padded_sequence(embedded, lengths, batch_first=True, enforce_sorted=False)
-       out, _ = self.rnn(packed)
-       unpacked, _ = pad_packed_sequence(out, batch_first=True)
-       out = self.linear(unpacked[:, -1, :])
-       return out
+# Note-Beam Prediction Module   
+class NBPModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers): # Initialization for NBPModel Class
+        super(NBPModel, self).__init__() # Initializes nn.Module to inherit the functions
+        # Long Short Term Memonry that gives the inputs tensors first dibs on batch dimension
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True) # Long Short Term Memory Layer
+        #Full Connected linear layer that turns the output from the LSTM layer which is the hiddensize into a single output
+        self.fc = nn.Linear(hidden_size, 1) # Linear Layer
+        # Sigmoid Activation, which creates probabilities from the output 0 or 1
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x): # Forward Pass of the Neural network
+        x = x.unsqueeze(1) # Takes input tensor X and add an extra dimension to match sequence for LSTM model
+        # Output has the hidden states for each LSTM time step, by passing the x tensor through the LSTM Layer, ignoring final hidden state
+        out, _ = self.lstm(x) 
+        # The output from the last time step in the LSTM is now passed in the connected layer  
+        out = self.fc(out[:, -1, :])
+        out = self.sigmoid(out).squeeze()  # Apply sigmoid activation for binary classification and remove single dimensions
+        return out # Return the prediction to be used in the DataLoader and trainer/evalution
 
 # Model Paramaters
 input_size =  7 # 7 Features(Measure Number,Pitch.midi, note duration in int, [Beam Start, Beam Continue, Beam Stop, Beam Partial(Left/Right)])
@@ -98,7 +118,7 @@ train_folder_path = "~/CS411-Crystal-Crossfade/Prototype/src/main/Python/edu/odu
 test_folder_path = "~/CS411-Crystal-Crossfade/Prototype/src/main/Python/edu/odu/cs/cs411/Backend/Incorrect_Beams" # path to mxl files containing incorrect beams format
 
 # Instantiate the model and define loss function and optimizer
-model = RNNModel(input_size, hidden_size, output_size) # Recurrent Neural Network to use sequential data and patterns to predict the most likely scenario
+model = RNNModel(input_size, hidden_size, output_size) # Neural Network Model to use sequential data and patterns to predict the most likely scenario
 criterion = nn.MSELoss() # Mean-Square Error Loss, best for regression. Outputs number P ∈[0,1]
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001) # Best default optimizer,default learning rate is 0.001, best results 0.002-0.003. If facing overfitting adjust learning rate.
 
@@ -110,30 +130,6 @@ test_dataset = MusicXMLDataset(test_folder_path)
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-#music_dataset = MusicXMLDataset(parse_musicxml('Control_Tests'))
-#dataloader = DataLoader(music_dataset, batch_size=batch_size, shuffle=True)
-
-"""""
-def train_model(model, data_loader):
-    for epoch in range(num_epochs):
-        for batch in data_loader:
-         optimizer.zero_grad()
-         sequences, lengths = zip(*batch)
-
-         x_batch = pad_sequence([torch.tensor(seq) for seq in sequences], batch_first=True)
-         lengths = torch.tensor(lengths)
-         y_batch = torch.randn((x_batch.size(0), output_size))
-         
-         outputs = model(x_batch, lengths)
-         loss = criterion(outputs, y_batch)
-         loss.backward()
-         optimizer.step()
-         
-         if (batch+1) % 100 == 0:
-            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{batch+1}/{len(dataloader)}], Loss: {loss.item()}')
-
-#train_model(model, dataloader)
-"""
 # Train model using training dataset
 def train_model(model, train_loader):
     for epoch in range(num_epochs):
